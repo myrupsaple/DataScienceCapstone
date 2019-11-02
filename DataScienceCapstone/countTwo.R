376b9a4eec1a8376e3ed7eaf616c786d20e840ed
DESKTOP-9NLI0PH\Riley <myrupsaple@gmail.com>
2019-11-01 18:09:38 -0700

    Minor Change to All Functions

--- DataScienceCapstone/countTwo.R
+++ DataScienceCapstone/countTwo.R
@@ -1,4 +1,4 @@
-countTwo <- function(phrases){
+countTwo <- function(phrases, log = TRUE){
         library(hash)
         wordCounts <- hash()
         
@@ -6,23 +6,28 @@ countTwo <- function(phrases){
         times = vector()
         # Parse each phrase and then each pair of words from that phrase
         for (i in 1:length(phrases)){
+                ###################Function Status Printouts###################
                 # Printout data so you can have the peace of mind of knowing
                 # how close the function is to completing :)
-                freqUpdate <- 500
-                if(i %% freqUpdate == 0){
-                        print(paste("Analyzing: ", i, "/", length(phrases), ' (', 
-                                    round(i/length(phrases), 4)*100 , '%) ', "complete", sep = ''))
+                # Disable this part by setting argument log = False
+                freqUpdate <- 2000
+                if(log){
+                        if(i %% freqUpdate == 0){
+                                print(paste("Analyzing: ", i, "/", length(phrases), ' (', 
+                                            round(i/length(phrases), 4)*100 , '%) ', "complete", sep = ''))
+                        }
+                        freqReport <- 20000
+                        if(i %% freqReport == 0){
+                                time <- proc.time() - ptm
+                                time <- round(time[[1]], 4)
+                                timems <- time * 1000
+                                print(paste("Last ", freqReport, " :", timems, "ms"))
+                                times[i/freqReport] <- timems
+                                print(paste("Total:", sum(times)/1000, "s"))
+                                ptm <- proc.time()
+                        }
                 }
-                freqReport <- 2500
-                if(i %% freqReport == 0){
-                        time <- proc.time() - ptm
-                        time <- round(time[[1]], 4)
-                        timems <- time * 1000
-                        print(paste("Last ", freqReport, " :", timems, "ms"))
-                        times[i/freqReport] <- timems
-                        print(paste("Total:", sum(times)/1000, "s"))
-                        ptm <- proc.time()
-                }
+                ################################################################
                 if(phrases[i] == ''){
                         next
                 }
--- DataScienceCapstone/filterSpecial.R
+++ DataScienceCapstone/filterSpecial.R
@@ -1,12 +1,23 @@
-filterSpecial <- function(phrases, n = 10){
+filterSpecial <- function(phrases, n = 10, log = TRUE){
         filtered <- vector()
         skipped = 0
         set.seed(1234)
-        indices = sample(c(1:length(phrases)), length(phrases)/n, replace = FALSE)
+        if (n == 1){
+                indices = c(1:length(phrases))
+        }
+        else{
+                indices = sample(c(1:length(phrases)), length(phrases)/n, replace = FALSE)
+        }
         for (i in 1:length(indices)){
+                ###########################Function Status Printouts###################
+                # Printout data so you can have the peace of mind of knowing
+                # how close the function is to completing :)
+                # Disable this part by setting argument log = False
                 if(i %% 10000 == 0){
-                        print(paste("Processing:", i, "/", length(indices), "complete"))
+                        print(paste("Processing:", i, "/", length(indices), ' (', 
+                                    round(i/length(indices), 4)*100 , '%)' , "complete"), sep = '')
                 }
+                #######################################################################
                 index = indices[i]
                 if (class(phrases[index]) != 'character'){
                         skipped = skipped + 1
@@ -14,6 +25,7 @@ filterSpecial <- function(phrases, n = 10){
                 }
                 filtered[i - skipped] <- gsub("[[:punct:]]", "", phrases[index])
         }
+        
         print("Filtration Complete!")
         filtered
 }
