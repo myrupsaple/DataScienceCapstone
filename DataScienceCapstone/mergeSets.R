0c6549dd96e1c2ee1c29619a7a724817f468ab69
DESKTOP-9NLI0PH\Riley <myrupsaple@gmail.com>
2019-11-06 18:40:44 -0800

    Fixed whitespace error

--- DataScienceCapstone/mergeSets.R
+++ DataScienceCapstone/mergeSets.R
@@ -1,31 +1,19 @@
 mergeSets <- function(data1, data2, data3){
-        debug = TRUE # Disables output log
+        debug = FALSE # Disables output log
         library(hash)
         wordCounts <- hash()
         
         phrases <- rbind(data1, data2, data3)
         
-        freqUpdate <- ceiling(dim(phrases)[1]/100)
-        freqPercent <- 1
-        # Remove leading spaces from datasets.
-        for (i in 1:len){
-                if(i %% freqUpdate == 0 && debug == FALSE){
-                        print(paste0("Removing Leading Spaces: ",
-                                     freqPercent, '%', " complete"))
-                        freqPercent <- freqPercent + 1
-                }
-                
-                data[i , 1] <- gsub('^\\s', '', data[i, 1])
-        }
-        
         # May need to change '100' to something smaller for data sets that
         # contain less than 100 observations.
-        freqPercent <- 1
+        freqUpdate <- ceiling(dim(phrases)[1]/10)
+        freqPercent <- 10
         for (i in 1:dim(phrases)[1]){
                 if(i %% freqUpdate == 0 && debug == FALSE){
                         print(paste0("Analyzing: ",
                                      freqPercent, '%', " complete"))
-                        freqPercent <- freqPercent + 1
+                        freqPercent <- freqPercent + 10
                 }
                 currentPhrase <- phrases[i, 1]
                 nextWord <- phrases[i, 2]
@@ -74,12 +62,25 @@ mergeSets <- function(data1, data2, data3){
                 }
         }
         
+        freqUpdate <- ceiling(length(keyPhrases)/100)
+        freqPercent <- 1
+        # Remove leading spaces from datasets.
+        for (i in 1:length(keyWords)){
+                if(i %% freqUpdate == 0 && debug == FALSE){
+                        print(paste0("Removing Leading Spaces: ",
+                                     freqPercent, '%', " complete"))
+                        freqPercent <- freqPercent + 1
+                }
+                
+                keyWords[i] <- gsub('^\\s', '', keyWords[i])
+        }
+        print('Finished removing leading whitespaces')
+        
         tempCount <- vector()
         for (i in 1:length(counts)){
                 tempCount[i] <- counts[[i]]
         }
         
-        merged <- data.frame(keyWords, followingWords, tempCount)
         merged <- merged[order(-counts), ]
         rownames(merged) <- c()
         colnames(merged) <- c('Leading_Phrase', 'Next_Word', 'Count')
